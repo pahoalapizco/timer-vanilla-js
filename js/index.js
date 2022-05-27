bc2ed77f6c74f92bbc685291db97f585933a5303
Paola Alapizco <paolac.alapizco@gmail.com>
2022-05-27 13:46:34 -0600

    Change active buttons and title

--- js/index.js
+++ js/index.js
@@ -1,95 +1,30 @@
-const spanHours = document.getElementById("hours");
-const spanMinutes = document.getElementById("minuts");
-const spanSeconds = document.getElementById("seconds");
-const milisecondsSpan = document.getElementById("miliseconds");
-const playButton = document.getElementById('play-button');
-const pauseButton = document.getElementById('pause-button');
+/* Button actions */
 
-let timeInterval;
-let hoursValue = 0,
-    minutesValue = 0
-    secondsValue = 0;
-    milisecondsValue = 0;
+const btnChronometer = document.getElementById("time-chronometer");
+const btnTimer = document.getElementById("time-timer");
+const btnPomodoro = document.getElementById("time-pomodoro");
+const maintTittle = document.getElementById("timer-tittle");
 
-let play = true;
-let newPlay = true;
+const onClickChronometer = () => {
+  btnChronometer.classList.add('active');
+  btnTimer.classList.remove('active');
+  btnPomodoro.classList.remove('active');
 
-const format = (value) => ('0'+value).slice(-2);
-const reset = () => {  
-  if(newPlay) {
-    hoursValue = 0,
-    minutesValue = 0
-    secondsValue = 0;
-    milisecondsValue = 0;
-  
-    spanHours.textContent = '00';
-    spanMinutes.textContent = '00';
-    spanSeconds.textContent = '00';
-    milisecondsSpan.textContent = '00';
-  }
-};
-
-const clearTimerInterval = () => {
-  if(timeInterval) {
-    clearInterval(timeInterval);
-  }
+  maintTittle.innerText = "Chronometer";
 }
 
-const start = () => {
-  timeInterval = setInterval(() => {
-    milisecondsValue++;
-    milisecondsSpan.textContent = format(milisecondsValue);
-    
-    if(milisecondsValue === 60) {
-      milisecondsValue = 0;
-      secondsValue++;
-      spanSeconds.textContent = format(secondsValue);
-    }
+const onClickTimer = () => {
+  btnTimer.classList.add('active');
+  btnChronometer.classList.remove('active');
+  btnPomodoro.classList.remove('active');
 
-    if(secondsValue === 60) {
-      secondsValue = 0;
-      minutesValue++;
-      spanMinutes.textContent = format(minutesValue);
-    }
-    if(minutesValue === 60) {
-      minutesValue = 0;
-      hoursValue++,
-      spanHours.textContent = format(hoursValue);
-    }
-  }, 100);
-}
-const removeAddClasses = (showPlay = true) => {
-  if(showPlay) {
-    playButton.classList.remove('timer--button__hiden');
-    pauseButton.classList.add('timer--button__hiden');
-  } else {
-    playButton.classList.add('timer--button__hiden');
-    pauseButton.classList.remove('timer--button__hiden');
-  }
+  maintTittle.innerText = "Timer";
 }
 
-const onClickPlayPause = () => {
-  if(play) {
-    reset();
-    start();
-  } else {
-    clearTimerInterval();
-    newPlay = false;
-  }
-  play = !play;
-  removeAddClasses(play);
-}
+const onClickPomodoro = () => {
+  btnPomodoro.classList.add('active');
+  btnTimer.classList.remove('active');
+  btnChronometer.classList.remove('active');
 
-const onClickStop = () => {
-  removeAddClasses(true);
-  reset();
-  clearTimerInterval();
-  newPlay = true;
-}
-
-const onClickRestart = () => {
-  removeAddClasses(false);
-  clearTimerInterval();
-  reset();
-  start();
+  maintTittle.innerText = "Pomodoro";
 }
